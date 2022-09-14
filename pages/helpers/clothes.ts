c1d29e21c46fd0d530812f0f697a884cf68cd69a
Jean <zikvanz@gmail.com>
2022-09-14 18:57:47 -0500

    feat(Index): andir listado de ropas

---
+++ pages/helpers/clothes.ts
@@ -0,0 +1,47 @@
+import image1 from '../../public/images/clothe1.jpg';
+import image2 from '../../public/images/clothe2.jpg';
+import image3 from '../../public/images/clothe3.jpg';
+import image4 from '../../public/images/clothe4.jpg';
+import image5 from '../../public/images/clothe5.jpg';
+import image6 from '../../public/images/clothe6.jpg';
+
+const CLOTHES = [
+  {
+    id: 1,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image1,
+  },
+  {
+    id: 2,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image2,
+  },
+  {
+    id: 3,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image3,
+  },
+  {
+    id: 4,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image4,
+  },
+  {
+    id: 5,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image5,
+  },
+  {
+    id: 6,
+    name: 'Capa roja y azul',
+    price: 'S./ 150',
+    image: image6,
+  },
+];
+
+export default CLOTHES;
