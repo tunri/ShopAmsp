c9ca554b2abbd754096d1868c0b17a016963f040
ericvipo <ericvpoma@gmail.com>
2022-09-24 23:55:03 -0500

    feat: Información de producto

--- pages/product/[pid].tsx
+++ pages/product/[pid].tsx
@@ -3,6 +3,9 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import ButtonLoading from "../../components/ButtonLoading";
 
 const ProductDetail = () => {
 	const router = useRouter();
@@ -14,13 +17,61 @@ const ProductDetail = () => {
 				<Grid container spacing={4}>
 					<Grid item xs={12} md={6}>
 						<Box>
-                                
-                        </Box>
+              <img src="https://i.imgur.com/tTEplrf.jpeg" alt="" />
+            </Box>
 					</Grid>
 					<Grid item xs={12} md={6}>
 						<Box>
-
-                        </Box>
+							<Typography variant="h4" mb={1}>
+								Nombre del producto
+							</Typography>
+							<Typography variant="body1" mb={1}>
+								Descripción del producto
+							</Typography>
+							<Typography
+								variant="body1"
+								mb={2}
+								sx={{ fontWeight: 700 }}
+							>
+								$500
+							</Typography>
+							<Stack direction="row" spacing={1} mb={2}>
+								<Typography variant="body2" color="#777">
+									Color:
+								</Typography>
+								<Typography variant="body2">White</Typography>
+							</Stack>
+							<Stack direction="row" spacing={1}>
+								<Typography variant="body2" color="#777">
+									Size:
+								</Typography>
+								<Typography variant="body2">Medium</Typography>
+							</Stack>
+							<Typography variant="body1" mb={3} mt={2}>
+								Descripción de la talla
+							</Typography>
+							<ButtonLoading
+								variant="contained"
+								color="primary"
+								fullWidth
+								size="large"
+								sx={{ height: 40 }}
+								type="submit"
+							>
+								Add to Bag
+							</ButtonLoading>
+              <Box my={2}/>
+              <ButtonLoading
+								variant="outlined"
+								color="primary"
+								fullWidth
+								size="large"
+								sx={{ height: 40 }}
+								type="submit"
+							>
+								Add to Wish List
+							</ButtonLoading>
+						</Box>
 					</Grid>
 				</Grid>
 			</Container>
